3c3bee97a1757d0fe90042253896f78f9179914c
Dylan <dylan.reeder5@gmail.com>
2022-03-21 20:42:19 -0400

    initial

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext _context;
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetAllProducts()
+        {
+            var products = _context.Products.ToList();
+            return Ok(products);
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstApi.Models
+{
+    public class Category
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Product> Products { get; set; }
+    }
+}
